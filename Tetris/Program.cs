205e295e136701e7d0e7f48a1d3b894140fec31f
Berze Zsofia <sophia_vivienne@icloud.com>
2014-04-10 19:52:16 +0200

    Tetris
    az

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -9,6 +9,19 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.CursorVisible = false;
+
+            Console.Title = "Tetris ||by: Sophie";
+
+            Console.WriteLine("Irányítás:\n");
+            Console.WriteLine("↑ - Letesz");
+            Console.WriteLine("↓ - Lentebb visz"); 
+            Console.WriteLine("→ - Jobbra visz");
+            Console.WriteLine("← - Balra visz");
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            Console.ReadKey();
         }
     }
 }
